f8bc3f5b6200e5ee9f7c30b35b95330655869da8
Talhakhan17 <Talhakhan01051996@gmail.com>
2026-06-20 23:37:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id="parent">
+      <h1 id='child'>Hello World</h1>
+      <h2>Hello </h2>
+
+      
+    </div>
+  )
+  
+}
+
+export default App
