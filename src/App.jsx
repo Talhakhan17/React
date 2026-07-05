a32c980fc421fd13687a381035a4a8b9125654a1
Talhakhan17 <Talhakhan01051996@gmail.com>
2026-07-05 15:00:04 +0530

    initial commit for ui-design-proj

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// import React from 'react'
+
+import Sec1 from "./Components/Section1/Sec1";
+import Sec2 from "./Components/Section2/Sec2";
+// import Sec1 from "./Components/Section1/Navbar"
+const App = () => {
+  const users = [
+    {
+      id: 1,
+      img: "https://plus.unsplash.com/premium_photo-1673976275849-986056b83cae?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fHdvcmtpbmd8ZW58MHx8MHx8fDA%3D",
+      intro: "",
+      tag: "Satisfied",
+    },
+    {
+      id: 2,
+      img: "https://plus.unsplash.com/premium_photo-1661769159995-f3af0089875f?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8d29ya2luZ3xlbnwwfHwwfHx8MA%3D%3D",
+      intro: "",
+      tag: "Underserved",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/flagged/photo-1575227057258-50cb9bffb1af?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fHdvcmtpbmd8ZW58MHx8MHx8fDA%3D",
+      intro: "",
+      tag: "Whatever",
+    },
+  ];
+
+  return (
+    <div>
+      <Sec1 users={users} />
+      <Sec2 />
+    </div>
+  );
+};
+
+export default App;
